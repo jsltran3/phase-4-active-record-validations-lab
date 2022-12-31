49141fd85c5f088eccd9d9d3e957fda2fe2f7abf
Joseph <jsltran@gmail.com>
2022-12-30 18:28:31 -0800

    commit tree 88a71b614bdbc4f2a57fc1bea72149767d34c17b

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
+
+    # validates_uniqueness_of :name, :case_sensitive => false
+    validates :phone_number, length: { is: 10 }
+    
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,34 @@
 class Post < ApplicationRecord
+    validate :clickbait?
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    CLICKBAIT_WORDS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+    CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+    ]
+
+    # def clickbait?
+    #     if :CLICKBAIT_WORDS.none? { |wrd| wrd.match title }
+    #         errors.add(:title, "must be clickbait")
+    #     end 
+    # end
+    
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
